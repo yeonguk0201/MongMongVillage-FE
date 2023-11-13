4ae17e9e57c6e7d97a60ef3f40e5d67fa7bc0f47
최정윤 <ej921004@naver.com>
2023-11-14 03:15:42 +0900

    Feat : add LoginPage

---
+++ client/src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,14 @@
+import { Footer, Header } from '../../components';
+import { Container, Title } from './styles';
+
+const LoginPage = () => {
+  return (
+    <Container>
+      <Header />
+      <Title>로그인페이지</Title>
+      <Footer />
+    </Container>
+  );
+};
+
+export default LoginPage;
---
+++ client/src/pages/LoginPage/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 100px;
+`;
+
+export const Title = styled.h1`
+  color: white;
+  background-color: #ffd100;
+  font-size: 26px;
+  text-align: center;
+`;
