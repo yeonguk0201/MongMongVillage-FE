b6f5688da29096c7a7eced8c2f8197134d2b428c
최정윤 <ej921004@naver.com>
2023-11-14 03:15:29 +0900

    Feat : add MainPage

---
+++ client/src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,14 @@
+import { Header, Footer } from '../../components';
+import { Container, Title } from './styels';
+
+const MainPage = () => {
+  return (
+    <Container>
+      <Header />
+      <Title>메인페이지</Title>
+      <Footer />
+    </Container>
+  );
+};
+
+export default MainPage;
