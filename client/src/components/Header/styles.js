651ff2ba6a8ec48a752a00148b3ec51ad7db35c5
최정윤 <ej921004@naver.com>
2023-11-14 03:16:40 +0900

    Feat : add Header

---
+++ client/src/components/Header/styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: white;
+  box-shadow: 5px 5px 10px 0px lightgray;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 95px;
+  z-index: 10;
+`;
+
+export const Content = styled.p`
+  font-size: 20px;
+  text-align: center;
+`;
+
+export const Nav = styled.nav`
+  cursor: pointer;
+  text-align: center;
+`;
