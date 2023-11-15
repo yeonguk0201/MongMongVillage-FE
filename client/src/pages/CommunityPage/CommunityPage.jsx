8fb52c6ff8bc9cd94d0e0a8f42ef172cc8f562d9
JiHyeong <selfown711@gmail.com>
2023-11-15 10:42:04 +0900

    📌 Init: CommunityPage 생성
    CommunityPage 생성

---
+++ client/src/pages/CommunityPage/CommunityPage.jsx
@@ -0,0 +1,14 @@
+import { Footer, Header } from '../../components';
+import { Container, Title } from './styles';
+
+const LoginPage = () => {
+  return (
+    <Container>
+      <Header />
+      <Title>커뮤니티 페이지</Title>
+      <Footer />
+    </Container>
+  );
+};
+
+export default LoginPage;
