025206d54ba612a3621c0ad022c556ba5095ea64
영욱 <yeonguk0201@gmail.com>
2023-11-15 13:44:24 +0900

    Feat: 헤더, 메인 이미지추가

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,12 +1,47 @@
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Container, Content, Nav } from './styles';
+import {
+  Container,
+  Navbar,
+  Navitem,
+  LoginSignupMypage,
+  Login,
+  Signup,
+  Mypage,
+} from './styles';
+import Logo from '../Logo/Logo';
+
 const Header = () => {
   const navigate = useNavigate();
 
   return (
     <Container>
-      <Content>헤더입니다.</Content>
-      <Nav onClick={() => navigate('/login')}>로그인</Nav>
+      <Logo onClick={() => navigate('/')} />
+      <Navbar>
+        <Navitem id="home" onClick={() => navigate('/')}>
+          홈
+        </Navitem>
+        <Navitem id="community" onClick={() => navigate('/community')}>
+          커뮤니티
+        </Navitem>
+        <Navitem id="review" onClick={() => navigate('/review')}>
+          리뷰
+        </Navitem>
+        <Navitem id="information" onClick={() => navigate('/information')}>
+          카페정보
+        </Navitem>
+      </Navbar>
+      <LoginSignupMypage>
+        <Login id="login" onClick={() => navigate('/login')}>
+          로그인
+        </Login>
+        <Signup id="signup" onClick={() => navigate('/signup')}>
+          회원가입
+        </Signup>
+        <Mypage id="mypage" onClick={() => navigate('/mypage')}>
+          마이페이지
+        </Mypage>
+      </LoginSignupMypage>
     </Container>
   );
 };
--- client/src/pages/MainPage/MainPage.jsx
+++ client/src/pages/MainPage/MainPage.jsx
@@ -1,10 +1,11 @@
-import { Header, Footer } from '../../components';
+import { Header, Footer, Banner } from '../../components';
 import { Container, Title } from './styels';
 
 const MainPage = () => {
   return (
     <Container>
       <Header />
+      <Banner />
       <Title>메인페이지</Title>
       <Footer />
     </Container>
