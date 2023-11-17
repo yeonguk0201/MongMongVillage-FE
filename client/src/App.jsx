f4eb86e3e65b3fb89ec12e0c152f42a0e0071358
JiHyeong Lee <selfown711@gmail.com>
2023-11-17 09:09:36 +0000

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,14 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import {
-  LoginPage,
-  MainPage,
-  CommunityPage,
-  CommunityDetailPage,
-  NewPostPage,
-} from './pages';
+import { Route, Routes } from 'react-router-dom';
+import { ROUTE_ARR } from './routes/Routes';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/community" element={<CommunityPage />} />
-        <Route path="/community/:id" element={<CommunityDetailPage />} />
-        <Route path="/community/newpost" element={<NewPostPage />} />
-      </Routes>
-    </BrowserRouter>
-
+    <Routes>
+      {ROUTE_ARR.map((el) => (
+        <Route path={el.path} key={el.path} element={el.element} />
+      ))}
+    </Routes>
+  );
 }
 
 export default App;
