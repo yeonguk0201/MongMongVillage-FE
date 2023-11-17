5d20e3b90c1e7aa27be3680aef355b86884f38a5
yoonkyung <kyung3098@naver.com>
2023-11-18 01:56:44 +0900

    Fix: 폴더 구조 수정2

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 
-const connect = require('./schemas');
+const connect = require('./db');
 
 const app = express();
 
