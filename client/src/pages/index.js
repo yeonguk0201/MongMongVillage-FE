4b224d7c0af3b4b3863021294bd837bff5c96406
최정윤 <ej921004@naver.com>
2023-11-19 00:54:04 +0900

    Feat : add pages export

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,3 +2,5 @@ export * from './MainPage';
 export * from './LoginPage';
 export * from './CommunityPage';
 export * from './SignUpPage';
+export * from './MyPage';
+export * from './MyActivityPage';
