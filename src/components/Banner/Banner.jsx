1ce6f690ef411cfb1303612c1db23e3871ef328f
영욱 <yeonguk0201@gmail.com>
2023-12-01 22:23:58 +0900

    Fix: 배너 사진 추가

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -10,11 +10,15 @@ import {
   BtnImg,
   HoverContainer,
 } from './styles';
+import { ROUTE } from '../../routes/Routes';
+import { useNavigate } from 'react-router';
 import Slider from 'react-slick';
 import './slick.css';
 import './slick-theme.css';
 
 const Banner = () => {
+  const navigate = useNavigate();
+
   const settings = {
     className: 'center',
     centerMode: true,
@@ -36,7 +40,7 @@ const Banner = () => {
           <BannerImg src="/banner.png" />
           <BannerName>
             멍멍 <br />{' '}
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;빌리지1
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;빌리지
           </BannerName>
 
           <BannerSub>서울의 모든 애견카페</BannerSub>
@@ -57,124 +61,34 @@ const Banner = () => {
           </HoverContainer>
         </BannerBox>
         <BannerBox>
-          <BannerImg src="/banner.png" />
-          <BannerName>
-            멍멍 <br />{' '}
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;빌리지2
-          </BannerName>
-
-          <BannerSub>서울의 모든 애견카페</BannerSub>
-
-          <BannerInfo>
-            서울의 모든 애견카페 리뷰와<br></br>애견카페에 관한 다양한 정보를
-            확인하세요!
-          </BannerInfo>
-
-          <BannerPhoneNumber>mongmongv6@gmail.com</BannerPhoneNumber>
-          <HoverContainer>
-            <PopolarCafeBtn
-              onClick={() => window.scrollTo({ top: 850, behavior: 'smooth' })}
-            >
-              눌러서 인기 애견카페 확인!
-            </PopolarCafeBtn>
-            <BtnImg src="/imges/btnlogo.png" alt="사진"></BtnImg>
-          </HoverContainer>
-        </BannerBox>{' '}
+          <BannerImg src="/banner2.png" />
+        </BannerBox>
         <BannerBox>
-          <BannerImg src="/banner.png" />
-          <BannerName>
-            멍멍 <br />{' '}
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;빌리지3
-          </BannerName>
-
-          <BannerSub>서울의 모든 애견카페</BannerSub>
-
-          <BannerInfo>
-            서울의 모든 애견카페 리뷰와<br></br>애견카페에 관한 다양한 정보를
-            확인하세요!
-          </BannerInfo>
-
-          <BannerPhoneNumber>mongmongv6@gmail.com</BannerPhoneNumber>
-          <HoverContainer>
-            <PopolarCafeBtn
-              onClick={() => window.scrollTo({ top: 850, behavior: 'smooth' })}
-            >
-              눌러서 인기 애견카페 확인!
-            </PopolarCafeBtn>
-            <BtnImg src="/imges/btnlogo.png" alt="사진"></BtnImg>
-          </HoverContainer>
-        </BannerBox>{' '}
+          <BannerImg src="/banner3.png" />
+        </BannerBox>
         <BannerBox>
-          <BannerImg src="/banner.png" />
-          <BannerName>
-            멍멍 <br />{' '}
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;빌리지4
-          </BannerName>
-
-          <BannerSub>서울의 모든 애견카페</BannerSub>
-
-          <BannerInfo>
-            서울의 모든 애견카페 리뷰와<br></br>애견카페에 관한 다양한 정보를
-            확인하세요!
-          </BannerInfo>
-
-          <BannerPhoneNumber>mongmongv6@gmail.com</BannerPhoneNumber>
-          <HoverContainer>
-            <PopolarCafeBtn
-              onClick={() => window.scrollTo({ top: 850, behavior: 'smooth' })}
-            >
-              눌러서 인기 애견카페 확인!
-            </PopolarCafeBtn>
-            <BtnImg src="/imges/btnlogo.png" alt="사진"></BtnImg>
-          </HoverContainer>
-        </BannerBox>{' '}
+          <BannerImg src="/banner4.png" />
+        </BannerBox>
         <BannerBox>
-          <BannerImg src="/banner.png" />
-          <BannerName>
-            멍멍 <br />{' '}
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;빌리지5
-          </BannerName>
-
-          <BannerSub>서울의 모든 애견카페</BannerSub>
-
-          <BannerInfo>
-            서울의 모든 애견카페 리뷰와<br></br>애견카페에 관한 다양한 정보를
-            확인하세요!
-          </BannerInfo>
-
-          <BannerPhoneNumber>mongmongv6@gmail.com</BannerPhoneNumber>
-          <HoverContainer>
-            <PopolarCafeBtn
-              onClick={() => window.scrollTo({ top: 850, behavior: 'smooth' })}
-            >
-              눌러서 인기 애견카페 확인!
-            </PopolarCafeBtn>
-            <BtnImg src="/imges/btnlogo.png" alt="사진"></BtnImg>
-          </HoverContainer>
-        </BannerBox>{' '}
+          <BannerImg src="/banner5.png" />
+          <PopolarCafeBtn
+            style={{
+              left: '270px',
+              width: '680px',
+              height: '70px',
+              fontSize: '35px',
+              backgroundColor: '#FF814C',
+            }}
+            onClick={() => navigate(ROUTE.CAFE_MAP_PAGE.link)}
+          >
+            눌러서 강아지랑 같이 갈 애견카페 검색하러 가기!
+          </PopolarCafeBtn>
+        </BannerBox>
         <BannerBox>
-          <BannerImg src="/banner.png" />
-          <BannerName>
-            멍멍 <br />{' '}
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;빌리지6
+          <BannerImg src="/banner6.png" />
+          <BannerName style={{ left: '283px', bottom: '260px' }}>
+            Mong Mong Village
           </BannerName>
-
-          <BannerSub>서울의 모든 애견카페</BannerSub>
-
-          <BannerInfo>
-            서울의 모든 애견카페 리뷰와<br></br>애견카페에 관한 다양한 정보를
-            확인하세요!
-          </BannerInfo>
-
-          <BannerPhoneNumber>mongmongv6@gmail.com</BannerPhoneNumber>
-          <HoverContainer>
-            <PopolarCafeBtn
-              onClick={() => window.scrollTo({ top: 850, behavior: 'smooth' })}
-            >
-              눌러서 인기 애견카페 확인!
-            </PopolarCafeBtn>
-            <BtnImg src="/imges/btnlogo.png" alt="사진"></BtnImg>
-          </HoverContainer>
         </BannerBox>
       </Slider>
     </Container>
