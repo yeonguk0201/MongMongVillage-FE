101eb18f4265a8f51a70710c588fbf4b03065726
최정윤 <ej921004@naver.com>
2023-12-29 05:28:42 +0900

    Feat : apply responsive design at SignupPage

--- src/components/SignUpCheckbox/styles.jsx
+++ src/components/SignUpCheckbox/styles.jsx
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 100%;
   margin: 20px;
+
+  @media screen and (max-width: 767px) {
+    margin: 10px;
+  }
 `;
 
 export const CheckboxContainer = styled.div`
@@ -18,6 +22,9 @@ export const CheckboxText = styled.p`
   margin: 10px;
   font-size: 16px;
   cursor: pointer;
+  @media screen and (max-width: 767px) {
+    font-size: 12px;
+  }
 `;
 
 export const Space = styled.div`
@@ -29,4 +36,8 @@ export const DetailText = styled.span`
   text-decoration: underline;
   margin: 0 3px;
   cursor: pointer;
+  @media screen and (max-width: 767px) {
+    font-size: 12px;
+    margin: 0 2px;
+  }
 `;
